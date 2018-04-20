fbad6ebf08beb169efa6f16ad0986410a79f88f8
Floren HENRI <fhenri@e1r7p8.42.fr>
2018-04-20 12:22:52 +0200

    [ADD]doc

--- server/test/test1_User.js
+++ server/test/test1_User.js
@@ -9,7 +9,7 @@ import jwt from 'jsonwebtoken'
 // global.tokenAdmin = ''
 // global.tokenUser = ''
 const serverUrl = 'http://localhost:8080'
-var user = { firstName: 'un', password: 'fwefewfewfewfewfewfewfew', lastName: 'TonPere', email: 'oeijgoiewjg@feoigheiowj.com', id: 0 }
+var user = { firstName: 'un', password: 'fwefewfewfewfewfewfewfew', lastName: 'TonPere', email: 'oeijgoiewjg@feoigheiowj.com',  }
 var token = ""
 
 chai.should()
@@ -21,9 +21,8 @@ describe('Test of UserController', () => {
 			.send(user)
 			.set('Accept', 'application/json')
 			.then((res) => {
-				console.log('\n\nuser', user, '\n\n\n')
+
 				user = jwt.decode(res.body.token)
-				console.log(user)
 				token = res.body.token
 				assert.equal(res.statusCode, '200')
 				done()
@@ -42,7 +41,7 @@ describe('Test of UserController', () => {
 	})
 	it('Update Public User', (done) => {
 
-		endpoint = `/user/update/${user.id}`
+		endpoint = `user/update/${user.id}`
 		user.lastName = 'okok'
 		request.post(`${serverUrl}/${endpoint}`)
 			.send(user)
@@ -53,19 +52,4 @@ describe('Test of UserController', () => {
 				done()
 			})
 	})
-	it('Update Public User', (done) => {
-
-		endpoint = `/user/update/private/${user.id}`
-		user.lastName = 'koko'
-		request.post(`${serverUrl}/${endpoint}`)
-			.send(user)
-			.set('Accept', 'application/json')
-			.set('X-Access-Token', token || '')
-			.then((res) => {
-				assert.equal(res.statusCode, '200')
-				done()
-			})
-	})
 })
-
-
